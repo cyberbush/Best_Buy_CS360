3e7f33e5fdc570d8ce4d55dca3845378d6cea9cd
Roun6273 <113474248+Roun6273@users.noreply.github.com>
2022-10-19 13:44:59 -0700

    Test Upload
    added a  piece of text to user page to verify that it uploads correctly. more changes coming tonight =)

--- client/src/components/User.vue
+++ client/src/components/User.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <h2>This is the User's home page</h2>
+    <p> This is a test upload from Tracy</p>
     <br>
     <a href="/"><u>Go back to Home</u></a>
   </div>
