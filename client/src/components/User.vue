cf35afeb99a4529d8b36641e5a4f44a30ad3128c
Roun6273 <113474248+Roun6273@users.noreply.github.com>
2022-10-21 03:22:38 -0700

    Initial layout of User.vue added
    inital layout of User,vue added. Will be improving in the next day, but for now, wanted to give some progress update =)

--- client/src/components/User.vue
+++ client/src/components/User.vue
@@ -1,8 +1,82 @@
 <template>
-  <div class="container">
-    <h2>This is the User's home page</h2>
-    <p> This is a test upload from Tracy</p>
-    <br>
-    <a href="/"><u>Go back to Home</u></a>
-  </div>
+
+  <!--
+    THINGS TO FINSIH 10-21-22
+  1. banner picture
+  2. header rows
+  3. figure out how to import from DBMS
+  4. Finish nav-bar design
+  5. Login system
+  -->
+  <body class="bg-dark">
+    <div :style="{'background-image':'url(https://vuejs.org/images/logo.png)'}" class="">
+      <!--Upper Nav-Bar-->
+      <div class="bg-dark ">
+          <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarNavDarkDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Menu
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarNavDarkDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </div>
+                </li>
+              </ul>
+              <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-light my-2 my-sm-0 bg-secondary" type="submit">Search</button>
+              </form>
+            </div>
+          </nav>
+        </div>
+        <!--Banner Goes Here-->
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+      <!--main body columns and rows-->
+      <div class="row bg-0">
+        <!--Featured Product/service area-->
+        <div class="col-8 text-lg-center border-primary bg-primary">
+          <!--ADD LITTLE ROW FOR HEADERS!!!-->
+          <h2 class="bg-dark">Featured Product and/or Service!</h2>
+          
+            <p>Talent she for lively eat led sister
+              Satisfied conveying an dependent contented he gentleman agreeable do be. 
+            Warrant private blushes removed an in equally totally if. Delivered dejection necessary objection do mr prevailed. Mr feeling do chiefly cordial in do. Water timed folly right aware if oh truth. Imprudence attachment him his for sympathize. Large above be to means. Dashwood do provided stronger is. But discretion frequently sir the she instrument unaffected admiration everything.</p><p>Affronting imprudence do he he everything. Sex lasted dinner wanted indeed wished out law. Far advanced settling say finished raillery. Offered chiefly farther of my no colonel shyness. Such on help ye some door if in. Laughter proposal laughing any son law consider. Needed except up piqued an.</p><p>Remain valley who mrs uneasy remove wooded him you. Her questions favourite him concealed. We to wife face took he. The taste begin early old why since dried can first. Prepared as or humoured formerly. Evil mrs true get post. Express village evening prudent my as ye hundred forming. Thoughts she why not directly reserved packages you. Winter an silent favour of am tended mutual.</p><p>Are sentiments apartments decisively the especially alteration. Thrown shy denote ten ladies though ask saw. Or by to he going think order event music. Incommode so intention defective at convinced. Led income months itself and houses you. After nor you leave might share court balls.</p><p>Ladies others the six desire age. Bred am soon park past read by lain. As excuse eldest no moment. An delight beloved up garrets am cottage private. The far attachment discovered celebrated decisively surrounded for and. Sir new the particular frequently indulgence excellence how. Wishing an if he sixteen visited tedious subject it. Mind mrs yet did quit high even you went. Sex against the two however not nothing prudent colonel greater. Up husband removed parties staying he subject mr.</p><p>Arrival entered an if drawing request. How daughters not promotion few knowledge contented. Yet winter law behind number stairs garret excuse. Minuter we natural conduct gravity if pointed oh no. Am immediate unwilling of attempted admitting disposing it. Handsome opinions on am at it ladyship.</p><p>No depending be convinced in unfeeling he. Excellence she unaffected and too sentiments her. Rooms he doors there ye aware in by shall. Education remainder in so cordially. His remainder and own dejection daughters sportsmen. Is easy took he shed to kind.</p><p>Subjects to ecstatic children he. Could ye leave up as built match. Dejection agreeable attention set suspected led offending. Admitting an performed supposing by. Garden agreed matter are should formed temper had. Full held gay now roof whom such next was. Ham pretty our people moment put excuse narrow. Spite mirth money six above get going great own. Started now shortly had for assured hearing expense. Led juvenile his laughing speedily put pleasant relation offering.</p><p>In on announcing if of comparison pianoforte projection. Maids hoped gay yet bed asked blind dried point. On abroad danger likely regret twenty edward do. Too horrible consider followed may differed age. An rest if more five mr of. Age just her rank met down way.
+             Attended required so in cheerful an. Domestic replying she resolved him for did. Rather in lasted no within no. </p>
+        </div>
+        <!--Based on cart recomendations-->
+        <div class="col-4 text-lg-center border-primary bg-danger">
+          <!--ADD LITTLE ROW FOR HEADERS!!!-->
+          <p> Products based on your current cart</p>
+        </div>
+      </div>
+      <!--Individuals cart-->
+      <div class="row bg-success">
+        <!--ADD LITTLE ROW FOR HEADERS!!!-->
+        <p> Your cart!</p>
+      </div>
+    </div>
+  </body>
+    
+
+
 </template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
 Vue.config.productionTip = false;
 
