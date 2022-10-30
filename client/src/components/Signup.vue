5c5644cbd9f2d57075b26f6cfc519610c8221f97
Roun6273 <113474248+Roun6273@users.noreply.github.com>
2022-10-30 01:36:05 -0700

    upload of Signup page for troubleshooting
    Trying to get signups going for logins. Issues have been posted in the discord for help from David - on backend / app.py starting issues

--- client/src/components/Signup.vue
+++ client/src/components/Signup.vue
@@ -8,40 +8,45 @@
                 <div class="card-body p-5">
                   <label class="text-uppercase text-center mb-5 text-dark">Create an account</label>
     
-                  <form class="text-center">
+                  <form @submit="submit_data" class="text-center">
     
-                    <div class="form-outline mb-3">
-                      <input type="text" id="form3Example1cg" class="form-control form-control-lg" />
-                      <label class="form-label text-dark" for="form3Example1cg" placeholder="First and Last Name">Your Name</label>
+                    <div class="form-outline mb-2 mt-2">
+                      <input type="text" id="form3Example1cg" class="form-control form-control " v-model="first_name" placeholder="First Name" />
+                      <label class="form-label text-dark" for="form3Example1cg" placeholder="First name">First Name</label>
+                    </div>
+                    <div class="form-outline mb-2 mt-2">
+                      <input type="text" id="form3Example1cg" class="form-control form-control" v-model="last_name" placeholder="Last Name" />
+                      <label class="form-label text-dark" for="form3Example1cg" placeholder="First and Last Name">Last Name</label>
                     </div>
     
-                    <div class="form-outline mb-3">
-                      <input type="email" id="form3Example3cg" class="form-control form-control-lg" />
+                    <div class="form-outline mb-2 amt-2">
+                      <input type="email" id="form3Example3cg" class="form-control form-control" v-model="email" placeholder="Email"/>
                       <label class="form-label text-dark" for="form3Example3cg">Your Email</label>
                     </div>
     
-                    <div class="form-outline mb-3">
-                      <input type="password" id="form3Example4cg" class="form-control form-control-lg" />
+                    <div class="form-outline mb-2 mt-2">
+                      <input type="password" id="form3Example4cg" class="form-control form-control" v-model="password" placeholder="Password"/>
                       <label class="form-label text-dark" for="form3Example4cg">Password</label>
                     </div>
     
-                    <div class="form-outline mb-3">
-                      <input type="password" id="form3Example4cdg" class="form-control form-control-lg" />
+                    <div class="form-outline mb-2 mt-2">
+                      <input type="password" id="form3Example4cdg" class="form-control form-control" v-model="password_confirm" placeholder="Confirm Password" />
                       <label class="form-label text-dark" for="form3Example4cdg">Repeat your password</label>
                     </div>
-    
                     <div class="form-check d-flex justify-content-center mb-3">
-                      <input class="form-check-input mb-2" type="checkbox" value="" id="form2Example3cg" />
+                      
                       <label class="form-check-label text-dark mb-2" for="">
-                        I agree all statements in <a href="help" class="text-body"><u>Terms of service</u></a>
+                        I am associated with a service company: <a href="help" class="text-body"></a>
                       </label>
+                      <br>
+                      <input class="form-check-input mb-2" type="checkbox" value="" id="form2Example3cg" />
                     </div>
     
                     <div class="d-flex justify-content-center mb-3">
                       <button type="button" class="btn btn-success btn-block btn-lg gradient-custom-Signup1 text-light">Register</button>
                     </div>
     
-                    <p class="text-center text-muted mt-2 mb-0">Have already an account? <a href="/"
+                    <p class="text-center text-muted mt-2 mb-3">Have already an account? <a href="/"
                         class="fw-bold text-body"><u>Login here</u></a></p>
     
                   </form>
@@ -75,4 +80,32 @@
     /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
     background: linear-gradient(to right, #0660F3, #2458AD, #4F91FC, #C5D7F3)
     }
+
 </style>
+
+<script>
+export default {
+  name: 'Register',
+  data() {
+    return{
+      first_name:'',
+      last_name:'',
+      email:'',
+      password:'',
+      password_confirm:'',
+    }
+  },
+  methods:{
+    submit_data(){
+      const data ={
+        first_name: this.first_name,
+        last_name: this.last_name,
+        email: this.email,
+        password: password,
+        password_confirm:password_confirm,
+    };
+      console.log(data)
+    }
+  }
+};
+</script>
