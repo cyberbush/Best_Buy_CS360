1f7c786d2da3997dc64c42d11fe7e71fc4a17ea3
hollyk24 <89818862+hollyk24@users.noreply.github.com>
2022-11-10 13:40:54 -0800

    Revert "Produce_view changes!"
    This reverts commit 4431ec2c2870467fa5ad90cda8e4a6bc54979b07.

--- client/src/components/product_display.vue
+++ client/src/components/product_display.vue
@@ -1,9 +1,9 @@
 <template>
 
-<body class="h-100 gradient-custom-HomePage">
+<body class="bg-dark">
       <!--Upper Nav-Bar-->
       <div class="bg-dark">
-          <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark border border-primary ">
+          <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary ">
             <a class="navbar-brand" href="/"> Best Buy!
             </a>
             <ul class="navbar-nav">
@@ -28,231 +28,90 @@
                     <button class="btn btn-dark my-2 my-sm-0" type="submit">Search</button>
                 </form>
             </div>
-            <p class=""></p>
+            <p class="">Logged in Peoples!</p>
           </nav>
         </div>
-        <br>
         <!--END OF NAVIGATION BAR-->
 
-        <!--adjust column width ratios for many
-        also set JS so that if there is x amount of offers, it shows, 
-      otherwise dont show the offers!-->
-        <div class="container-fluid d-flex justify-content-center">
-          <div class="row mt-5">
-            <div class="col-sm-4">
-              <div class="card">
-                <img src="https://imgur.com/edOjtEC.png" class="card-img-top" width="100%">
-                <div class="card-body pt-0 px-0">
-                  <div class="d-flex flex-row justify-content-between mb-0 px-3">
-                    <small class="text-muted mt-1">STARTING AT</small>
-                    <h6>&dollar;22,495&ast;</h6>
-                  </div>
-                  <hr class="mt-2 mx-3">
-                  <div class="d-flex flex-row justify-content-between px-3 pb-4">
-                    <div class="d-flex flex-column"><span class="text-muted">Fuel Efficiency</span>
-                      <small class="text-muted">L/100KM&ast;</small>
-                    </div>
-                    <div class="d-flex flex-column"><h5 class="mb-0">8.5/7.1</h5>
-                      <small class="text-muted text-right">(city/Hwy)</small>
-                    </div>
-                    </div>
-                      <div class="d-flex flex-row justify-content-between p-3 mid">
-                        <div class="d-flex flex-column"><small class="text-muted mb-1">ENGINE</small>
-                          <div class="d-flex flex-row">
-                            <img src="https://imgur.com/iPtsG7I.png" width="35px" height="25px">
-                            <div class="d-flex flex-column ml-1">
-                              <small class="ghj">1.4L MultiAir</small>
-                              <small class="ghj">16V I-4 Turbo</small>
-                            </div>
-                          </div>
-                        </div>
-                        <div class="d-flex flex-column">
-                          <small class="text-muted mb-2">HORSEPOWER</small>
-                          <div class="d-flex flex-row">
-                            <img src="https://imgur.com/J11mEBq.png">
-                            <h6 class="ml-1">135 hp&ast;</h6>
-                          </div>
-                        </div>
-                      </div>
-                      <small class="text-muted key pl-3">Standard key Features</small>
-                      <div class="mx-3 mt-3 mb-2">
-                        <button type="button" style="background-color: #92FE9D; border: #92FE9D" class="btn btn-success btn-block">
-                          <small>BUILD & PRICE</small>
-                        </button>
-                      </div>
-                      <small class="d-flex justify-content-center text-muted">*Legal Disclaimer</small>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-sm-4">
-                  <div class="card">
-                    <img src="https://imgur.com/SKZolYE.png" class="card-img-top" width="100%">
-                      <div class="card-body pt-0 px-0">
-                        <div class="d-flex flex-row justify-content-between mb-0 px-3">
-                        <small class="text-muted mt-1">STARTING AT</small>
-                        <h6>&dollar;22,495&ast;</h6>
-                      </div>
-                      <hr class="mt-2 mx-3">
-                      <div class="d-flex flex-row justify-content-between px-3 pb-4">
-                        <div class="d-flex flex-column"><span class="text-muted">Fuel Efficiency</span>
-                          <small class="text-muted">L/100KM&ast;</small>
-                        </div>
-                        <div class="d-flex flex-column">
-                          <h5 class="mb-0">8.5/7.1</h5>
-                          <small class="text-muted text-right">(city/Hwy)</small>
-                        </div>
-                      </div>
-                      <div class="d-flex flex-row justify-content-between p-3 mid">
-                        <div class="d-flex flex-column"><small class="text-muted mb-1">ENGINE</small><div class="d-flex flex-row">
-                          <img src="https://imgur.com/iPtsG7I.png" width="35px" height="25px"><div class="d-flex flex-column ml-1">
-                          <small class="ghj">1.4L MultiAir</small>
-                          <small small class="ghj">16V I-4 Turbo</small>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="d-flex flex-column">
-                      <small class="text-muted mb-2">HORSEPOWER</small>
-                        <div class="d-flex flex-row">
-                          <img src="https://imgur.com/J11mEBq.png">
-                          <h6 class="ml-1">135 hp&ast;</h6>
-                        </div>        
-                      </div>
-                    </div>
-                      <small class="text-muted key pl-3">Standard key Features</small>
-                        <div class="mx-3 mt-3 mb-2"><button type="button" style="background-color: #92FE9D; border: #92FE9D" class="btn btn-success btn-block">
-                          <small>BUILD & PRICE</small>
-                          </button>
-                        </div>
-                        <small class="d-flex justify-content-center text-muted">*Legal Disclaimer</small>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="col-sm-4">
-                    <div class="card">
-                      <img src="https://imgur.com/SKZolYE.png" class="card-img-top" width="100%">
-                        <div class="card-body pt-0 px-0">
-                          <div class="d-flex flex-row justify-content-between mb-0 px-3">
-                          <small class="text-muted mt-1">STARTING AT</small>
-                          <h6>&dollar;22,495&ast;</h6>
-                        </div>
-                        <hr class="mt-2 mx-3">
-                        <div class="d-flex flex-row justify-content-between px-3 pb-4">
-                          <div class="d-flex flex-column"><span class="text-muted">Fuel Efficiency</span>
-                            <small class="text-muted">L/100KM&ast;</small>
-                          </div>
-                          <div class="d-flex flex-column">
-                            <h5 class="mb-0">8.5/7.1</h5>
-                            <small class="text-muted text-right">(city/Hwy)</small>
-                          </div>
-                        </div>
-                        <div class="d-flex flex-row justify-content-between p-3 mid">
-                          <div class="d-flex flex-column"><small class="text-muted mb-1">ENGINE</small><div class="d-flex flex-row">
-                            <img src="https://imgur.com/iPtsG7I.png" width="35px" height="25px"><div class="d-flex flex-column ml-1">
-                            <small class="ghj">1.4L MultiAir</small>
-                            <small small class="ghj">16V I-4 Turbo</small>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="d-flex flex-column">
-                        <small class="text-muted mb-2">HORSEPOWER</small>
-                          <div class="d-flex flex-row">
-                            <img src="https://imgur.com/J11mEBq.png">
-                            <h6 class="ml-1">135 hp&ast;</h6>
-                          </div>        
-                        </div>
-                      </div>
-                        <small class="text-muted key pl-3">Standard key Features</small>
-                          <div class="mx-3 mt-3 mb-2"><button type="button" style="background-color: #92FE9D; border: #92FE9D" class="btn btn-success btn-block">
-                            <small>BUILD & PRICE</small>
-                            </button>
-                          </div>
-                          <small class="d-flex justify-content-center text-muted">*Legal Disclaimer</small>
-                        </div>
-                      </div>
-                    </div>
-                  <div class="col-sm-4">
-                    <div class="card">
-                      <img src="https://imgur.com/gUQNKPd.png" class="card-img-top" width="100%">
-                        <div class="card-body pt-0 px-0">
-                          <div class="d-flex flex-row justify-content-between mb-0 px-3">
-                            <small class="text-muted mt-1">STARTING AT</small>
-                            <h6>&dollar;22,495&ast;</h6>
-                          </div>
-                          <hr class="mt-2 mx-3">
-                          <div class="d-flex flex-row justify-content-between px-3 pb-4">
-                            <div class="d-flex flex-column"><span class="text-muted">Fuel Efficiency</span>
-                              <small class="text-muted">L/100KM&ast;</small>
-                            </div>
-                              <div class="d-flex flex-column"><h5 class="mb-0">8.5/7.1</h5>
-                                <small class="text-muted text-right">(city/Hwy)</small>
-                              </div>
-                            </div>
-                            <div class="d-flex flex-row justify-content-between p-3 mid">
-                              <div class="d-flex flex-column"><small class="text-muted mb-1">ENGINE</small>
-                                <div class="d-flex flex-row"><img src="https://imgur.com/iPtsG7I.png" width="35px" height="25px">
-                                  <div class="d-flex flex-column ml-1"><small class="ghj">1.4L MultiAir</small>
-                                    <small class="ghj">16V I-4 Turbo</small>
-                                  </div>
-                                </div>
-                              </div>
-                            <div class="d-flex flex-column">
-                              <small class="text-muted mb-2">HORSEPOWER</small>
-                              <div class="d-flex flex-row">
-                                <img src="https://imgur.com/J11mEBq.png">
-                                <h6 class="ml-1">135 hp&ast;</h6>
-                              </div>
-                            </div>
-                          </div>
-                        <small class="text-muted key pl-3">Standard key Features</small>
-                      <div class="mx-3 mt-3 mb-2">
-                        <button type="button" style="background-color: #92FE9D; border: #92FE9D" class="btn btn-success btn-block">
-                          <small>BUILD & PRICE</small>
-                        </button>
-                      </div>
-                      <small class="d-flex justify-content-center text-muted">*Legal Disclaimer</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          
-              
-      
+
+<div class="container-fluid d-flex justify-content-center">
+  <div class="row mt-5">
+    <div class="col-sm-4">
+      <div class="card">
+    <img src="https://imgur.com/edOjtEC.png" class="card-img-top" width="100%">
+    <div class="card-body pt-0 px-0">
+      <div class="d-flex flex-row justify-content-between mb-0 px-3">
+        <small class="text-muted mt-1">STARTING AT</small>
+        <h6>&dollar;22,495&ast;</h6>
+      </div>
+      <hr class="mt-2 mx-3">
+      <div class="d-flex flex-row justify-content-between px-3 pb-4">
+        <div class="d-flex flex-column"><span class="text-muted">Fuel Efficiency</span><small class="text-muted">L/100KM&ast;</small></div>
+        <div class="d-flex flex-column"><h5 class="mb-0">8.5/7.1</h5><small class="text-muted text-right">(city/Hwy)</small></div>
+      </div>
+      <div class="d-flex flex-row justify-content-between p-3 mid">
+        <div class="d-flex flex-column"><small class="text-muted mb-1">ENGINE</small><div class="d-flex flex-row"><img src="https://imgur.com/iPtsG7I.png" width="35px" height="25px"><div class="d-flex flex-column ml-1"><small class="ghj">1.4L MultiAir</small><small class="ghj">16V I-4 Turbo</small></div></div></div>
+        <div class="d-flex flex-column"><small class="text-muted mb-2">HORSEPOWER</small><div class="d-flex flex-row"><img src="https://imgur.com/J11mEBq.png"><h6 class="ml-1">135 hp&ast;</h6></div></div>
+      </div>
+      <small class="text-muted key pl-3">Standard key Features</small>
+      <div class="mx-3 mt-3 mb-2"><button type="button" style="background-color: #92FE9D; border: #92FE9D" class="btn btn-success btn-block"><small>BUILD & PRICE</small></button></div>
+      <small class="d-flex justify-content-center text-muted">*Legal Disclaimer</small>
+    </div>
+  </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="card">
+    <img src="https://imgur.com/SKZolYE.png" class="card-img-top" width="100%">
+    <div class="card-body pt-0 px-0">
+      <div class="d-flex flex-row justify-content-between mb-0 px-3">
+        <small class="text-muted mt-1">STARTING AT</small>
+        <h6>&dollar;22,495&ast;</h6>
+      </div>
+      <hr class="mt-2 mx-3">
+      <div class="d-flex flex-row justify-content-between px-3 pb-4">
+        <div class="d-flex flex-column"><span class="text-muted">Fuel Efficiency</span><small class="text-muted">L/100KM&ast;</small></div>
+        <div class="d-flex flex-column"><h5 class="mb-0">8.5/7.1</h5><small class="text-muted text-right">(city/Hwy)</small></div>
+      </div>
+      <div class="d-flex flex-row justify-content-between p-3 mid">
+        <div class="d-flex flex-column"><small class="text-muted mb-1">ENGINE</small><div class="d-flex flex-row"><img src="https://imgur.com/iPtsG7I.png" width="35px" height="25px"><div class="d-flex flex-column ml-1"><small class="ghj">1.4L MultiAir</small><small class="ghj">16V I-4 Turbo</small></div></div></div>
+        <div class="d-flex flex-column"><small class="text-muted mb-2">HORSEPOWER</small><div class="d-flex flex-row"><img src="https://imgur.com/J11mEBq.png"><h6 class="ml-1">135 hp&ast;</h6></div></div>
+      </div>
+      <small class="text-muted key pl-3">Standard key Features</small>
+      <div class="mx-3 mt-3 mb-2"><button type="button" style="background-color: #92FE9D; border: #92FE9D" class="btn btn-success btn-block"><small>BUILD & PRICE</small></button></div>
+      <small class="d-flex justify-content-center text-muted">*Legal Disclaimer</small>
+    </div>
+  </div>
+    </div>
+  <div class="col-sm-4">
+    <div class="card">
+    <img src="https://imgur.com/gUQNKPd.png" class="card-img-top" width="100%">
+    <div class="card-body pt-0 px-0">
+      <div class="d-flex flex-row justify-content-between mb-0 px-3">
+        <small class="text-muted mt-1">STARTING AT</small>
+        <h6>&dollar;22,495&ast;</h6>
+      </div>
+      <hr class="mt-2 mx-3">
+      <div class="d-flex flex-row justify-content-between px-3 pb-4">
+        <div class="d-flex flex-column"><span class="text-muted">Fuel Efficiency</span><small class="text-muted">L/100KM&ast;</small></div>
+        <div class="d-flex flex-column"><h5 class="mb-0">8.5/7.1</h5><small class="text-muted text-right">(city/Hwy)</small></div>
+      </div>
+      <div class="d-flex flex-row justify-content-between p-3 mid">
+        <div class="d-flex flex-column"><small class="text-muted mb-1">ENGINE</small><div class="d-flex flex-row"><img src="https://imgur.com/iPtsG7I.png" width="35px" height="25px"><div class="d-flex flex-column ml-1"><small class="ghj">1.4L MultiAir</small><small class="ghj">16V I-4 Turbo</small></div></div></div>
+        <div class="d-flex flex-column"><small class="text-muted mb-2">HORSEPOWER</small><div class="d-flex flex-row"><img src="https://imgur.com/J11mEBq.png"><h6 class="ml-1">135 hp&ast;</h6></div></div>
+      </div>
+      <small class="text-muted key pl-3">Standard key Features</small>
+      <div class="mx-3 mt-3 mb-2"><button type="button" style="background-color: #92FE9D; border: #92FE9D" class="btn btn-success btn-block"><small>BUILD & PRICE</small></button></div>
+      <small class="d-flex justify-content-center text-muted">*Legal Disclaimer</small>
+    </div>
+  </div>
+    </div>
+  
+  </div>
+</div>
 
 </body>
 
 </template>
-
-<style>
-.gradient-custom-HomePage {
-  /* fallback for old browsers */
-  background: #4F91FC;
-  
-  /* Chrome 10-25, Safari 5.1-6 */
-  background: -webkit-linear-gradient(to right, #0660F3, #2458AD, #4F91FC, #7ebfef);
-  
-  /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-  background: linear-gradient(to right, #0660F3, #2458AD, #4F91FC, #7ebfef);
-  }
-  
-  @media (min-width: 768px) {
-  .gradient-form {
-  height: 100vh !important;
-  }
-  }
-  @media (min-width: 769px) {
-  .gradient-custom-HomePage {
-  border-top-right-radius: .3rem;
-  border-bottom-right-radius: .3rem;
-  }
-  }
-
-  .homepage_login{
-    background: whitesmoke;
-    
-  }
-  
-</style>
 
 <script>
 
