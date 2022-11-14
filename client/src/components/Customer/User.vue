5949e9aac62399f3ab0f78c478683943778bdc17
Roun6273 <113474248+Roun6273@users.noreply.github.com>
2022-11-14 01:05:09 -0800

    Updated User.vue..NEED VERIFICATION
    David, can you go through and check out my link to the product page, and make sure I made it right to be able to display the information from the products page? I will then update this to show cards with fake pictures until we can figure that out =)

--- client/src/components/Customer/User.vue
+++ client/src/components/Customer/User.vue
@@ -8,10 +8,10 @@
   4. Finish nav-bar design
   5. Login system
   -->
-  <body class="bg-dark">
+  <body class="gradient-custom-HomePage">
       <!--Upper Nav-Bar-->
-      <div class="bg-dark">
-          <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary ">
+      <div class="gradient-custom-HomePage">
+          <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-secondary">
             <a class="navbar-brand" href="/"> Best Buy!
             </a>
             <ul class="navbar-nav">
@@ -41,9 +41,6 @@
           </nav>
         </div>
         <!--BANNER SECTION-->
-        <div class="Banner_Container">
-          <img src="../Logo/Vendor_Logo.png" class="" height="150px" width="100%" alt="Matrix picture Banner">
-        </div>
         <!--END BANNER SECTION-->
         <!--main body columns and rows-->
 
@@ -54,63 +51,108 @@
         UPDATE 11/11/2022: redesign in process, linking products database to system, will have cards... looking for good ecommerse page layout
         as well =) DONT PANIC... and dont forget a towel!--> 
 
-        <div class="container">
-          <section class="mx-auto my-5" style="max-width: 23rem;">
-            <div class="card">
-              <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                <img src="https://mdbootstrap.com/img/Photos/Horizontal/Food/8-col/img (5).jpg" class="img-fluid" />
-                <a href="#!">
-                  <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
-                </a>
-              </div>
-              <div class="card-body">
-                <p class="card-title font-weight-bold text-dark"><a>Product name</a></p>
-                <ul class="list-unstyled list-inline mb-0">
-                  <li class="list-inline-item me-0">
-                    <i class="fas fa-star text-warning fa-xs"> </i>
-                  </li>
-                  <li class="list-inline-item me-0">
-                    <i class="fas fa-star text-warning fa-xs"></i>
-                  </li>
-                  <li class="list-inline-item me-0">
-                    <i class="fas fa-star text-warning fa-xs"></i>
-                  </li>
-                  <li class="list-inline-item me-0">
-                    <i class="fas fa-star text-warning fa-xs"></i>
-                  </li>
-                  <li class="list-inline-item">
-                    <i class="fas fa-star-half-alt text-warning fa-xs"></i>
-                  </li>
-                  <li class="list-inline-item">
-                    <p class="text-muted">4.5 (413)</p>
-                  </li>
-                </ul>
-                <p class="mb-2 text-dark">product price</p>
-                <p class="card-text text-dark">
-                  THIS IS BEING WORKED ON! TEST TEST TEST!!! - TJR 11/11/2022
-                </p>
-                <hr class="my-4" />
-                <p class="lead"><strong>Tonight's availability</strong></p>
-                <ul class="list-unstyled list-inline d-flex justify-content-between">
-                  <li class="list-inline-item me-0">
-                    <div class="chip me-0">5:30PM</div>
-                  </li>
-                  <li class="list-inline-item me-0">
-                    <div class="chip bg-secondary text-white me-0">7:30PM</div>
-                  </li>
-                  <li class="list-inline-item me-0">
-                    <div class="chip me-0">8:00PM</div>
-                  </li>
-                  <li class="list-inline-item me-0">
-                    <div class="chip me-0">9:00PM</div>
-                  </li>
-                </ul>
-                <a href="#!" class="btn btn-link link-secondary p-md-1 mb-0">Button</a>
-              </div>
-            </div>
-            
-          </section>
+        <!--Main layout-->
+  <main class="mt-2 pt-4">
+    <h3 class="text-center"> Featured Product:</h3>
+    <div class="container-fluid dark-grey-text mt-5">
+
+      <!--Grid row-->
+      <div class="row wow fadeIn border bg-white">
+
+        <!--Grid column-->
+        <div class="col-md-6 mb-4">
+
+          <img src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Products/14.jpg" class="img-fluid" alt="">
+
         </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-md-6 mb-4">
+
+          <!--Content-->
+          <div class="p-4">
+
+            <div class="mb-3">
+              <a href="">
+                <span class="badge purple mr-1 text-dark">Category 2</span>
+              </a>
+              <a href="">
+                <span class="badge blue mr-1 text-dark">New</span>
+              </a>
+              <a href="">
+                <span class="badge red mr-1 text-dark">Bestseller</span>
+              </a>
+            </div>
+
+            <p class="lead">
+              <span class="mr-1 text-dark">
+                <del>$200</del>
+              </span>
+              <span class="text-dark">$100</span>
+            </p>
+
+            <p class="lead font-weight-bold text-dark">Description</p>
+
+            <p class="text-dark">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolor suscipit libero eos atque quia ipsa
+              sint voluptatibus!
+              Beatae sit assumenda asperiores iure at maxime atque repellendus maiores quia sapiente.</p>
+
+            <form class="d-flex justify-content-left mt-12">
+              <!-- Default input -->
+              <button class="btn btn-primary btn-md my-5 p" type="submit">Add to cart
+                <i class="fas fa-shopping-cart ml-1"></i>
+              </button>
+
+            </form>
+
+          </div>
+          <!--Content-->
+
+        </div>
+        <!--Grid column-->
+
+      </div>
+      <!--Grid row-->
+
+      <hr>
+
+      <!--Grid row-->
+      <div class="row d-flex justify-content-center wow fadeIn">
+
+        <!--Grid column-->
+        <div class="col-md-6 text-center mb-3">
+
+          <h4 class="my-4">More Products:</h4>
+          <!--WILL UPDATE THIS FOR PROJECT DEMO. DAVID OR HOLLY CAN YOU CHECK OUT THE SCRIPT BELOW TO SEE IF THIS 
+          MAKES SENSE? IT WAS NOT UPDATING, BUT THE ProductMenu.vue PAGE WAS? -->
+
+          <div id="productTable" class="productTable text-dark bg-light">
+            <ul>
+                <li v-for="product in products" :key="product.id"> 
+                {{ product.name + " | $" + product.price + " | " + product.description + " | " + product.category + " | " + product.status }}<button class="btn btn-primary" @click="update_product(id=product.id, name='', price=0.0, description='', category='', status='', delete_=true)">Delete</button>
+                </li>
+            </ul>
+        </div>
+
+
+        </div>
+      </div>
+    </div>
+  </main>
+  <!--Main layout-->
+
+  <!--Footer-->
+  <footer class="page-footer text-center font-small mt-4 wow fadeIn">
+
+    <!--Copyright-->
+    <div class="footer-copyright py-3">
+      Product not final! CS360 Best Buy! 
+      <a href="/" target="_blank">  </a>
+    </div>
+    <!--/.Copyright-->
+
+  </footer>
   </body>
     
 
@@ -119,7 +161,6 @@
 
 <script>
 //Tracy TO-DO:
-//-link products page - 11/11/2022
 export default {
   name: 'Register',
   data() {
@@ -133,6 +174,7 @@ export default {
     this.read_users();
   },
   methods: {
+    //USERS FUNCTIONS===========================================================
     load_user: function() {
       this.users.forEach( user => {
         if (user.id == this.$route.params.id) {
@@ -156,10 +198,84 @@ export default {
         .then(response => (this.users = response.data))
         .catch(error => { console.log(error.response) });
     },
+    //!USERS FUNCTIONS===========================================================
+    //PRODUCT FUNCTIONS===========================================================
+      //Taken from "ProductMenu.vue page"=====================
+    name: "product_comp",
+    data: function() {
+    return {
+      product_name: "",
+      product_price: "",
+      product_category: "",
+      product_desc: "",
+      product_status: "",
+      products: [
+        { id: 0, name: "", price: 0.00, description: "", category: "", status: ""},
+      ],
+    };
+  },
+  mounted: function() {
+    this.read_products();
+  },
+  computed: {
+  },
+  methods: {
+    // all the methods will be replaced with REST API call later
+    read_products: function() {
+        this.axios
+            .get("http://localhost:5000/api/products")
+            .then(response => (this.products = response.data))
+            .catch(error => { console.log(error.response) });
+    },
+    update_product: function( id = -1, name = "", price = 0.00, description = "", category= "", status = "", delete_ = false) 
+    {
+        var data = { id: id, name: name, price:price, description: description, category:category, status: status, delete: delete_ };
+        this.axios
+            .post("http://localhost:5000/api/products", data)
+            .then(() => this.read_products())
+            .catch(error => { console.log(error.response) });
+        //   add a delay to make sure the backend respond
+    },
+    print: function(data){
+        console.log(data)
+    }
+  }
+    //!PRODUCT FUNCTIONS===========================================================
   }
 };
+
 </script>
 
 <style>
+.gradient-custom-HomePage {
+  /* fallback for old browsers */
+  background: #4F91FC;
+  
+  /* Chrome 10-25, Safari 5.1-6 */
+  background: -webkit-linear-gradient(to right, #0660F3, #2458AD, #4F91FC, #7ebfef);
+  
+  /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  background: linear-gradient(to right, #0660F3, #2458AD, #4F91FC, #7ebfef);
+  }
+  
+  @media (min-width: 768px) {
+  .gradient-form {
+  height: 100vh !important;
+  }
+  }
+  @media (min-width: 769px) {
+  .gradient-custom-HomePage {
+  border-top-right-radius: .3rem;
+  border-bottom-right-radius: .3rem;
+  }
+  }
 
+  .productTable {
+    background-color: white;
+    margin: auto;
+    margin-top: 50px;
+    border: 6px solid #0AF;
+    padding: 50px;
+    text-align: center;
+}
 </style>
